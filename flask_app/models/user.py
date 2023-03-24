e0641004631d9b9ea28556232be97f2e49fb6755
Melissa Hernandez <mmdangelo11@gmail.com>
2023-03-24 15:26:09 -0700

    Updated templates and controllers

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -16,6 +16,7 @@ class User:
         self.password=data['password']
         self.created_at=data['created_at']
         self.updated_at=data['updated_at']
+
 # creat a user
     @classmethod
     def create_user(cls, data):
@@ -48,7 +49,7 @@ class User:
     def get_user_by_id(cls,data):
         query = "SELECT * FROM users WHERE id = %(id)s;"
         results = connectToMySQL(db).query_db(query,data)
-        user_data={
+        user_data = {
             "id":results[0]['id'],
             'first_name':results[0]['first_name'],
             'last_name':results[0]['last_name'],
@@ -61,7 +62,10 @@ class User:
     
     @classmethod
     def save(cls,data):
-        query="INSERT INTO users (first_name,last_name,email,password,created_at,updated_at) VALUES (%(first_name)s,%(last_name)s,%(email)s,%(password)s,NOW(),NOW());"
+        query="""
+            INSERT INTO users (first_name,last_name,email,password,created_at,updated_at) 
+            VALUES (%(first_name)s,%(last_name)s,%(email)s,%(password)s,NOW(),NOW());
+        """
         return connectToMySQL(db).query_db( query, data )
     
 # Validation
@@ -69,25 +73,26 @@ class User:
     def validate_user_reg_data(data):
         EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
         is_valid = True
-        if len(data['first_name'])<3:
-            flash("First name must be at least three character")
+        if len(data['first_name']) < 3:
+            flash("First name must be at least 3 characters")
             is_valid = False
-        if len(data['last_name'])<3:
-            flash("Last name must be at least three character")
+        if len(data['last_name']) < 3:
+            flash("Last name must be at least 3 characters")
             is_valid = False
-        if len(data['password'])<8:
-            flash("Your password must be at least 8 character")
+        if len(data['password']) < 8:
+            flash("Password must be at least 8 characters")
             is_valid = False
         if not EMAIL_REGEX.match(data['email']):
-            flash('Please use the correct  email.')
+            flash('Invalid email format')
             is_valid = False
         if User.get_user_by_email(data['email']):
-            flash('That email is already in use.')
+            flash('Email is already in use')
             is_valid = False
         if data['password'] != data['confirm_password']:
-            flash('Your passwords do not match')
+            flash('Passwords do not match')
             is_valid = False
         return is_valid
+
 # pasrse data
     @staticmethod
     def parse_reg_data(data):
@@ -97,6 +102,7 @@ class User:
         parsed_data['password'] = bcrypt.generate_password_hash(data['password'])
         parsed_data['email'] = data['email']
         return parsed_data
+
 # login validation
     @staticmethod
     def user_login(data):
@@ -105,4 +111,4 @@ class User:
             if bcrypt.check_password_hash(this_user.password, data['password']):
                 session['user_id'] = this_user.id
                 return True
-        flash('Your enter wrong information')
+        flash('Email and/or password incorrect')
